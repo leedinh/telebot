45cec857fd43683bc3a966ef89c296db4c2c8359
leedinh <ltddd3006@gmail.com>
2024-12-23 23:36:20 +0700

    add logger

--- bitly/cmd/url-shortener/main.go
+++ bitly/cmd/url-shortener/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	config "github.com/leedinh/telebot/bitly/internal/config/url-shortener"
 )
 
 func main() {
 	// Load the configuration
-	config := config.LoadConfig()
-	fmt.Println(config)
+	cfg := config.LoadConfig()
+	log := initLogger(cfg.Env)
+	log.Info("Starting the application")
 
+}
+
+func initLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case "local":
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case "dev":
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case "prod":
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
